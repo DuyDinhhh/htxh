9063b45b9b60dbb2829db7d6441955d3c107df3b
duydinhhh <duy.nguyentadinh@gmail.com>
2026-01-20 15:48:55 +0700

    fix conflict clientID

--- backend/app/Listeners/SkipNumberListener.php
+++ backend/app/Listeners/SkipNumberListener.php
@@ -26,9 +26,6 @@ class SkipNumberListener
     {
         try {
             $data = $event -> data;
-            $prefix = env('APP_ENV', "");
-            $clientId = 'publish-' . $data['device_id'].$prefix ;
-            config(['mqtt-client.connections.default.client_id' => $clientId]);
             
             $startOfDay = Carbon::today()->startOfDay();
             $endOfDay = Carbon::today()->endOfDay();   
